c676e6d184d091660e3a72fe1f6ab8bc1725827a
Adam Neve <afneve@gmail.com>
2017-02-05 19:21:20 -0600

    Adding Progress tracking

--- data/progress_data.js
+++ data/progress_data.js
@@ -1,49 +1,14 @@
-progress_data = [ {
-	"Segment": "Wood Lake",
-	"efnStatus" : "",
-	"afnStatus": "partial",
-	"notes" : "Completed the loop around the lake."
-}, {
-	"Segment": "Turtle Rock",
-	"efnStatus" : "done",
-	"afnStatus": "done"
-}, {
-	"Segment": "Grandfather Falls",
-	"efnStatus" : "partial",
-	"afnStatus": "done"
-}, {
-	"Segment": "Alta Junction",
-	"efnStatus" : "",
-	"afnStatus": "done"
-}, {
-	"Segment": "Plover River",
-	"efnStatus" : "partial",
-	"afnStatus": "done"
-}, {
-	"Segment": "Dells of the Eau Claire",
-	"efnStatus" : "partial",
-	"afnStatus": "done"
-}, {
-	"Segment": "Thornapple Creek",
-	"efnStatus" : "",
-	"afnStatus": "done"
-}, {
-	"Segment": "Ringle",
-	"efnStatus" : "",
-	"afnStatus": "done"
-}, {
-	"Segment": "Hartman Creek",
-	"efnStatus" : "partial",
-	"afnStatus" : "partial",
-	"notes" : "Completed section through the state park"
-},  {
-	"Segment": "Devil's Lake",
-	"efnStatus" : "partial",
-	"afnStatus": "partial",
-	"notes" : "Completed the segment in the South and around Devil's Lake."
-}, {
-	"Segment": "Sturgeon Bay",
-	"efnStatus" : "partial",
-	"afnStatus": "partial",
-	"notes" : "Completed the section through the state park"
-}]
+progress_data = {
+	"users" : [{
+		"user" : "Adam",
+		"userId" : "1",
+		"completedSegmentIds" : [33, 34, 36, 44, 45, 46, 47],
+		"partialSegmentIds" : [28, 51, 63, 120]
+	},
+	{
+		"user" : "Ernestine",
+		"userId" : "2",
+		"completedSegmentIds" : [33],
+		"partialSegmentIds" : [34, 44, 45, 51, 63, 120]
+	}]
+}
--- scripts/ice_age.js
+++ scripts/ice_age.js
@@ -29,19 +29,19 @@ var iceAge = {
             iceAge.getLabelsFromWebPage();
             iceAge.getJsonFromWebPage();
         } else {
-            if (navigator.geolocation) {
-                console.log(navigator.geolocation);
-                navigator.geolocation.getCurrentPosition(function(position){
-                    iceAge.position = position;
+           // if (navigator.geolocation) {
+             //   console.log(navigator.geolocation);
+             //   navigator.geolocation.getCurrentPosition(function(position){
+              //      iceAge.position = position;
                     iceAge.dataCollection();
                     iceAge.displaySegmentList();
-                }, function(){
-                    iceAge.dataCollection();
-                    iceAge.displaySegmentList();
-                });
-            } else { 
-                x.innerHTML = "Geolocation is not supported by this browser.";
-            }
+            //    }, function(){
+                   // iceAge.dataCollection();
+                  //  iceAge.displaySegmentList();
+             //   });
+           // } else { 
+           //     x.innerHTML = "Geolocation is not supported by this browser.";
+           // }
 
             
             //iceAge.formatData();
@@ -99,20 +99,19 @@ var iceAge = {
             difficulty = '',
             segmentCounter = 1;
 
-        //segment_id_location_data    
-
         for (var i = 0; i < ice_age_data.length; i++) {
             ice_age_data[i].segment_id = i + 1;
+            iceAge.totalTrailDistance += parseFloat(ice_age_data[i].iceagetraildistance);
+
             //IF NEW COUNTY
             if (ice_age_data[i].booksection != previousSection) {
                 if(i == 0){
                     segmentHTML += '<div class="county" data-index="'+ segmentCounter +'">';
-                    segmentCounter++
                 }
                 else{
                     segmentHTML += '<div class="county hide" data-index="'+ segmentCounter +'">';
-                    segmentCounter++
                 }
+                segmentCounter++
                 
                 segmentHTML += '<h3 id="segment_' + i + '">' + ice_age_data[i].booksection + '</h3>';
                 
@@ -123,54 +122,53 @@ var iceAge = {
                     filterHTML += '<li>';
                 }
                 
-
-
                 filterHTML += '<a href="' + (segmentCounter - 1) + '">' + ice_age_data[i].booksection + '</a>';
                 filterHTML += '</li>';
-                //filterHTML += '<ul>';
             }
-
-            //  filterHTML += '<li><a href="' + i + '">' + ice_age_data[i].segment + '</a></li>'
 
             segmentHTML += '<div class="segment_container">';
             segmentHTML += '<div class="segment" data-index="' + i + '">';
             segmentHTML += '<h4 class="segment_name">' + ice_age_data[i].segment + '</h4>';
             segmentHTML += '<div class="segment_summary">' + ice_age_data[i].summary + '</div>';
 
-//MESS TO CLEAN UP
-            if(parseFloat(ice_age_data[i].iceagetraildistance) <= iceAge.distanceObject.shortCutoff ){
-                difficulty = 'easy';
-            }
-            else if( parseFloat(ice_age_data[i].iceagetraildistance) <= iceAge.distanceObject.midCutoff ){
-                difficulty = 'average';
-            }
-            else{
-                difficulty = 'hard';
-            }
-            segmentHTML += '<div class="'+ difficulty +'">Distance: ' + ice_age_data[i].iceagetraildistance + '</div>';
+/*
+MESS TO CLEAN UP
+*/
+if(parseFloat(ice_age_data[i].iceagetraildistance) <= iceAge.distanceObject.shortCutoff ){
+    difficulty = 'easy';
+}
+else if( parseFloat(ice_age_data[i].iceagetraildistance) <= iceAge.distanceObject.midCutoff ){
+    difficulty = 'average';
+}
+else{
+    difficulty = 'hard';
+}
+segmentHTML += '<div class="'+ difficulty +'">Distance: ' + ice_age_data[i].iceagetraildistance + '</div>';
 
-            if(parseFloat(ice_age_data[i].elevation) <= iceAge.elevationObject.shortCutoff ){
-                difficulty = 'easy';
-            }
-            else if( parseFloat(ice_age_data[i].elevation) <= iceAge.elevationObject.midCutoff ){
-                difficulty = 'average';
-            }
-            else{
-                 difficulty = 'hard';
-            }
-            segmentHTML += '<div class="'+ difficulty +'">Elevation: ' + ice_age_data[i].elevation + '</div>';
+if(parseFloat(ice_age_data[i].elevation) <= iceAge.elevationObject.shortCutoff ){
+    difficulty = 'easy';
+}
+else if( parseFloat(ice_age_data[i].elevation) <= iceAge.elevationObject.midCutoff ){
+    difficulty = 'average';
+}
+else{
+        difficulty = 'hard';
+}
+segmentHTML += '<div class="'+ difficulty +'">Elevation: ' + ice_age_data[i].elevation + '</div>';
 
-            if(parseFloat(ice_age_data[i].ruggedness) <= iceAge.ruggednessObject.shortCutoff){
-                 difficulty = 'easy';
-            }
-            else if(parseFloat(ice_age_data[i].ruggedness) <= iceAge.ruggednessObject.midCutoff){
-                difficulty = 'average';
-            }
-            else{
-                 difficulty = 'hard';
-            }
-            segmentHTML += '<div class="'+ difficulty +'">Ruggedness: ' + ice_age_data[i].ruggedness + '</div>';
-//MESS TO CLEAN UP
+if(parseFloat(ice_age_data[i].ruggedness) <= iceAge.ruggednessObject.shortCutoff){
+        difficulty = 'easy';
+}
+else if(parseFloat(ice_age_data[i].ruggedness) <= iceAge.ruggednessObject.midCutoff){
+    difficulty = 'average';
+}
+else{
+        difficulty = 'hard';
+}
+segmentHTML += '<div class="'+ difficulty +'">Ruggedness: ' + ice_age_data[i].ruggedness + '</div>';
+/*
+MESS TO CLEAN UP
+*/
 
             segmentHTML += '<div class="icons">';
 
@@ -210,8 +208,7 @@ var iceAge = {
                         segmentHTML += '<a class="location" target="_blank" href="https://www.google.com/maps/dir/' + iceAge.position.coords.latitude + '+' + iceAge.position.coords.longitude + '/' + eastLat + 'N+' + eastLong + 'W">To East end from your location</a>';
                         segmentHTML += '<a class="location" target="_blank" href="https://www.google.com/maps/dir/' + iceAge.position.coords.latitude + '+' + iceAge.position.coords.longitude + '/' + westLat + 'N+' + westLong + 'W">To West end from your location</a>';
                     }
-                    
-                    //https://www.google.com/maps/dir/45°+23.979'+-92°+38.973'/45.4500167,-92.6488167
+                
                     segmentHTML += '</div>';
                 }
             }
@@ -220,16 +217,12 @@ var iceAge = {
                 segmentHTML += '<div class="nohiking">Hiking Restrictions: </div><div>' + ice_age_data[i].nohiking + '</div>';
             }
 
-            
-
             segmentHTML += '</div>';
             segmentHTML += '<div class="clear"></div>';
             //segmentHTML += '<span data-efnprogress="' + efnProgress + '" data-notes="'+notes+'"></span><span data-afnprogress="' + afnProgress + '" data-notes="'+notes+'"></span>';
             segmentHTML += '</div>';
+            segmentHTML += '<div class="clear"></div>';
             segmentHTML += '</div>';
-
-
-
 
             if (typeof ice_age_data[i + 1] != 'undefined') {
                 nextSection = ice_age_data[i + 1].booksection;
@@ -239,7 +232,9 @@ var iceAge = {
 
             if (ice_age_data[i].booksection != nextSection) {
                 //filterHTML += '</ul>';
+               
                 segmentHTML += '</div>'; //END COUNTY DIV
+                segmentHTML += '<div class="clear"></div>';
             }
 
             previousSection = ice_age_data[i].booksection;
@@ -247,10 +242,108 @@ var iceAge = {
 
         $('#segment_list').html(segmentHTML);
         $('#segment_filter ul').html(filterHTML);
+
+        iceAge.displayUserProgress();
     },
     /*
     ******************
-    Attach Event Listenrs
+    Display User Progress
+    ******************
+    */
+    displayUserProgress: function(){
+        var userName = '',
+            userHTML = '',
+            userCompleteList = '',
+            userPartialList = '',
+            userIncompleteList = '',
+            userCompleteMiles = 0,
+            userPartialMiles = 0,
+            userMilesRemaining = 0;
+
+        //segment_id_location_data  
+
+        for(var i=0; i < progress_data.users.length; i++){ 
+            userCompleteList = '';
+            userPartialList = '';
+            userCompleteMiles = 0;
+            userPartialMiles = 0;
+
+            userName = progress_data.users[i].user;
+            userHTML += '<div class="user_container">';
+            userHTML += '<h4>User ' + progress_data.users[i].userId + '</h4>';
+            for (var j = 0; j < ice_age_data.length; j++) {
+
+               
+                    for(var k=0; k < progress_data.users[i].completedSegmentIds.length; k++){
+                        var id = progress_data.users[i].completedSegmentIds[k];
+                        if(id == ice_age_data[j].segment_id){
+                            
+                            userCompleteList += '<div class="seg">' + ice_age_data[j].segment + '</div>';
+                            userCompleteMiles += parseFloat(ice_age_data[j].iceagetraildistance);                            
+                            break;
+                        }
+                    } 
+                  
+                    for(var l=0; l < progress_data.users[i].partialSegmentIds.length; l++){
+                        var id = progress_data.users[i].partialSegmentIds[l];
+                        if(id == ice_age_data[j].segment_id){
+                            userPartialList += '<div class="seg">' + ice_age_data[j].segment + '</div>';
+                            userPartialMiles += parseFloat(ice_age_data[j].iceagetraildistance);
+                            break;
+                        } 
+                    } 
+                
+            } //END ICE AGE DATA LOOP
+            userHTML += '<div class="user_segments">';
+            userHTML += '<div class="user_header">Completed Segments ( ' + progress_data.users[i].completedSegmentIds.length + ' )</div>';
+            userHTML += userCompleteList;
+            userHTML += '</div>';
+
+            userHTML += '<div class="user_segments">';
+            userHTML += '<div class="user_header">Partially Completed Segments ( ' + progress_data.users[i].partialSegmentIds.length + ' )</div>';
+            userHTML += userPartialList;
+            userHTML += '</div>';
+
+            userHTML += '<div class="user_segments">';
+            userHTML += '<div class="user_header">Distance:</div>';
+            userHTML += '<div>Length of trails partially done is ' + userPartialMiles + ' miles</div>';
+            userHTML += '<div>' + parseFloat(userCompleteMiles.toFixed(2)) + ' of ' + iceAge.totalTrailDistance + ' miles completed</div>';
+            console.log(iceAge.totalTrailDistance, userCompleteMiles);
+
+
+            userHTML += '<div>' + (parseFloat(iceAge.totalTrailDistance) - parseFloat(userCompleteMiles.toFixed(2))) + ' miles remaining!</div>'; 
+            userHTML += '</div>'
+
+            userHTML += '</div>';
+        } //END USER LOOP
+
+        $('#progress_container').html(userHTML);
+
+                /*if(progress_data[j].segment == ice_age_data[i].segment){
+                    console.log(ice_age_data[i].segment);
+                    console.log(ice_age_data[i].segment_id);
+
+                   // efnProgress = progress_data[j].efnStatus;
+                    //afnProgress = progress_data[j].afnStatus;
+
+                    //Name
+                    //Miles complete vs mile remaining
+                    //Number of completed segments
+                    //List of completed segments
+                    //Number of partial segments
+                    //List of partial segments
+                    //Number of unwalked segments
+                    //LIst of unwalked segments
+
+                    //progress_data.splice(j,1);            
+                }*/
+            
+        
+
+    },
+    /*
+    ******************
+    Attach Event Listeners
     ******************
     */
     attachEventListeners: function() {
@@ -267,6 +360,17 @@ var iceAge = {
             $('.county').hide();
             $('[data-index="' + segment + '"]').show();
 
+        });
+
+        $('#segments').on('click', function(e){
+            $('#progress_container').hide();
+            $('#segment_container').show();
+            
+        });
+
+        $('#progress').on('click', function(e){
+            $('#segment_container').hide();
+            $('#progress_container').show();
         });
 
         $('body').on('keyup', function(e) {
