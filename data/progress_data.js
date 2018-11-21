a914a6a28e931bf6c9fad63569bd3f0581cfdb60
Adam <afneve@gmail.com>
2018-11-20 19:13:58 -0600

    Fixing data issue

--- data/progress_data.js
+++ data/progress_data.js
@@ -6,181 +6,181 @@ progress_data = {
 		"completedSegments" : [
 			{
 				"segmentId" : 36,
-				"dateOfCompletion" : "2018-11-03"
+				"extraInfo" : "2018-11-03"
 			},
 			{
 				"segmentId" : 34,
-				"dateOfCompletion" : "2018-11-03"
+				"extraInfo" : "2018-11-03"
 			},
 			{
 				"segmentId" : 44,
-				"dateOfCompletion" : "2018-11-03"
+				"extraInfo" : "2018-11-03"
 			},
 			{
 				"segmentId" : 65,
-				"dateOfCompletion" : "2018-10-13"
+				"extraInfo" : "2018-10-13"
 			},
 			{
 				"segmentId" : 66,
-				"dateOfCompletion" : "2018-10-13"
+				"extraInfo" : "2018-10-13"
 			},
 			{
 				"segmentId" : 67,
-				"dateOfCompletion" : "2018-10-13"
+				"extraInfo" : "2018-10-13"
 			},
 			{
 				"segmentId" : 68,
-				"dateOfCompletion" : "2018-10-13"
+				"extraInfo" : "2018-10-13"
 			},
 			{
 				"segmentId" : 69,
-				"dateOfCompletion" : "2018-10-13"
+				"extraInfo" : "2018-10-13"
 			},
 			{
 				"segmentId" : 115,
-				"dateOfCompletion" : "2018-09-29"
+				"extraInfo" : "2018-09-29"
 			},
 			{
 				"segmentId" : 21,
-				"dateOfCompletion" : "2018-06-25"
+				"extraInfo" : "2018-06-25"
 			},
 			{
 				"segmentId" : 76,
-				"dateOfCompletion" : "2018-03-26"
+				"extraInfo" : "2018-03-26"
 			},
 			{
 				"segmentId" : 77,
-				"dateOfCompletion" : "2018-03-26"
+				"extraInfo" : "2018-03-26"
 			},
 			{
 				"segmentId" : 78,
-				"dateOfCompletion" : "2018-03-25"
+				"extraInfo" : "2018-03-25"
 			},
 			{
 				"segmentId" : 79,
-				"dateOfCompletion" : "2018-03-25"
+				"extraInfo" : "2018-03-25"
 			},
 			{
 				"segmentId" : 12,
-				"dateOfCompletion" : "2018-02-17"
+				"extraInfo" : "2018-02-17"
 			},
 			{
 				"segmentId" : 60,
-				"dateOfCompletion" : "2018-01-26"
+				"extraInfo" : "2018-01-26"
 			},
 			{
 				"segmentId" : 61,
-				"dateOfCompletion" : "2018-01-26"
+				"extraInfo" : "2018-01-26"
 			},
 			{
 				"segmentId" : 64,
-				"dateOfCompletion" : "2018-01-26"
+				"extraInfo" : "2018-01-26"
 			},
 			{
 				"segmentId" : 56,
-				"dateOfCompletion" : "2018-01-20"
+				"extraInfo" : "2018-01-20"
 			},
 			{
 				"segmentId" : 57,
-				"dateOfCompletion" : "2018-01-20"
+				"extraInfo" : "2018-01-20"
 			},
 			{
 				"segmentId" : 58,
-				"dateOfCompletion" : "2018-01-20"
+				"extraInfo" : "2018-01-20"
 			},
 			{
 				"segmentId" : 59,
-				"dateOfCompletion" : "2017-10-21"
+				"extraInfo" : "2017-10-21"
 			},
 			{
 				"segmentId" : 62,
-				"dateOfCompletion" : "2017-10-21"
+				"extraInfo" : "2017-10-21"
 			},
 			{
 				"segmentId" : 63,
-				"dateOfCompletion" : "2017-10-21"
+				"extraInfo" : "2017-10-21"
 			},
 			{
 				"segmentId" : 53,
-				"dateOfCompletion" : "2017-08-19"
+				"extraInfo" : "2017-08-19"
 			},
 			{
 				"segmentId" : 54,
-				"dateOfCompletion" : "2017-08-19"
+				"extraInfo" : "2017-08-19"
 			},
 			{
 				"segmentId" : 38,
-				"dateOfCompletion" : "2017-04-08"
+				"extraInfo" : "2017-04-08"
 			},
 			{
 				"segmentId" : 118,
-				"dateOfCompletion" : "2017-03-27"
+				"extraInfo" : "2017-03-27"
 			},
 			{
 				"segmentId" : 120,
-				"dateOfCompletion" : "2017-03-27"
+				"extraInfo" : "2017-03-27"
 			},
 			{
 				"segmentId" : 119,
-				"dateOfCompletion" : "2017-03-26"
+				"extraInfo" : "2017-03-26"
 			},
 			{
 				"segmentId" : 50,
-				"dateOfCompletion" : "2017-03-19"
+				"extraInfo" : "2017-03-19"
 			},
 			{
 				"segmentId" : 51,
-				"dateOfCompletion" : "2017-03-19"
+				"extraInfo" : "2017-03-19"
 			},
 			{
 				"segmentId" : 52,
-				"dateOfCompletion" : "2017-03-19"
+				"extraInfo" : "2017-03-19"
 			},
 			{
 				"segmentId" : 15,
-				"dateOfCompletion" : "2017-03-05"
+				"extraInfo" : "2017-03-05"
 			},
 			{
 				"segmentId" : 14,
-				"dateOfCompletion" : "2017-03-04"
+				"extraInfo" : "2017-03-04"
 			},
 			{
 				"segmentId" : 13,
-				"dateOfCompletion" : "2017-03-04"
+				"extraInfo" : "2017-03-04"
 			},
 			{
 				"segmentId" : 48,
-				"dateOfCompletion" : "2017-02-19"
+				"extraInfo" : "2017-02-19"
 			},
 			{
 				"segmentId" : 49,
-				"dateOfCompletion" : "2017-02-19"
+				"extraInfo" : "2017-02-19"
 			},
 			{
 				"segmentId" : 45,
-				"dateOfCompletion" : "2017-02-11"
+				"extraInfo" : "2017-02-11"
 			},
 			{
 				"segmentId" : 33,
-				"dateOfCompletion" : "2016-04-16"
+				"extraInfo" : "2016-04-16"
 			}
 		],
 		"partialSegments" : [
 			{
 				"segmentId" : 117,
-				"notes" : "Completed Western segment from B to Bb"
+				"extraInfo" : "Completed Western segment from B to Bb"
 			},
 			{
 				"segmentId" : 16,
-				"notes" : "Completed Western segment to Stout Road."
+				"extraInfo" : "Completed Western segment to Stout Road."
 			},
 			{
 				"segmentId" : 17,
-				"notes" : "Completed North section to about 2 miles North of North Lake. Stopped where trail left snowmobile trail on the East side"
+				"extraInfo" : "Completed North section to about 2 miles North of North Lake. Stopped where trail left snowmobile trail on the East side"
 			},
 			{
 				"segmentId" : 47,
-				"notes" : "Stopped at poplar road"
+				"extraInfo" : "Stopped at poplar road"
 			}
 		]
 	}]
